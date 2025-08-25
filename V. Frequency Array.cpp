6db46bbdcb26e569b6ebc81bd1903306639b88a5
Foysal79 <onthor312001@gmail.com>
2025-08-25 14:49:38 +0600

    V. Frequency Array

--- V. Frequency Array.cpp
+++ V. Frequency Array.cpp
@@ -2,21 +2,41 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
 
-    vector<int> freq(m, 0); // frequency vector for numbers 1 to M
 
-    for(int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        freq[x - 1]++; // increase count for number x
-    }
-
-    // print frequencies
-    for(int i = 0; i < m; i++) {
-        cout << freq[i] << endl;
-    }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+//* solve useing vector  
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n, m;
+//     cin >> n >> m;
+
+//     vector<int> freq(m, 0); // frequency vector for numbers 1 to M
+
+//     for(int i = 0; i < n; i++) {
+//         int x;
+//         cin >> x;
+//         freq[x-1]++; // increase count for number x
+//     }
+
+//     // print frequencies
+//     for(int i = 0; i < m; i++) {
+//         cout << freq[i] << endl;
+//     }
+
+//     return 0;
+// }
